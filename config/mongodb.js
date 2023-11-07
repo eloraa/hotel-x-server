51ce80259d41176cbd9be140c0d854367ec54dd9
elora <108570316+eloraistired@users.noreply.github.com>
2023-11-07 18:48:08 +0000

    init

---
+++ config/mongodb.js
@@ -0,0 +1,31 @@
+const logger = require("./logger");
+const { mongo, dbname } = require("./vars");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const client = new MongoClient(mongo.uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+
+
+/**
+ * Connect to mongo db
+ */
+exports.connect = () => {
+    client
+        .connect()
+        .then(() =>client.db(dbname).command({ ping: 1 }))
+        .then(() => logger.info("mongoDB connected..."))
+
+    return client;
+};
+
+/**
+* returns client
+*/
+exports.client =  client;
+exports.db =  client.db(dbname);
