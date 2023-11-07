7dd2a52caccf8070cb854dccf22fd2a826a3ff73
elora <108570316+eloraistired@users.noreply.github.com>
2023-11-07 19:09:48 +0000

    added the newsletter route

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const newletterroute = require('./newsletter.route');
 
 /**
  * GET v1/status
  */
 router.get('/status', (req, res) => res.send('OK'));
 
+// ROUTES
+router.use('/newsletter', newletterroute)
 
 
 module.exports = router;
